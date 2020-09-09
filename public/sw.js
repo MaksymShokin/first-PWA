1e0d7baae755d1a0df501b4bdd1b654f7cb10624
Maksym Shokin <maksym.sh@upplabs.com>
2020-09-09 09:16:18 +0300

    listening-to-fetch

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,7 @@ self.addEventListener('activate', event => {
   console.log('Activating SW', event);
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', event => {
+  console.log('Fetcing SW', event);
+});
